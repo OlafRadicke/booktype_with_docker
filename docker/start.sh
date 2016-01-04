aac9da6043f2b24433be92771dae9840febedfad
Olaf Radicke <o.radicke@meteocontrol.de>
2016-01-04 10:31:30 +0100

    Fixes in start script

--- docker/start.sh
+++ docker/start.sh
@@ -9,7 +9,7 @@ django-admin.py createsuperuser
 #django-admin.py createsuperuser --username='sysadmin' --email='sysadmin@okfn.org' --noinput
 
 # This has to be done every time you want to start a server
-cd /docker_booktype
+#cd /docker_booktype
 source /docker_booktype/bin/activate
 source ${instancename}/booktype.env
-django-admin.py runserver 0.0.0.0:8080
+django-admin.py /docker_booktype/runserver 0.0.0.0:8080
