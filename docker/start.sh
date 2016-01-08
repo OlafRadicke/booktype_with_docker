cfdb94414366560a8a935f9b4dab44d797457baf
Olaf Radicke <o.radicke@meteocontrol.de>
2016-01-08 10:41:32 +0100

    Move init staps to dockerfile.

--- docker/start.sh
+++ docker/start.sh
@@ -3,15 +3,6 @@
 echo "DJANGO_SETTINGS_MODULE:"
 echo $DJANGO_SETTINGS_MODULE
 
-# Initialise Booktype
-source ${instancename}/booktype.env
-django-admin.py syncdb --noinput
-django-admin.py migrate
-django-admin.py createsuperuser
-#django-admin.py createsuperuser --username='sysadmin' --email='sysadmin@okfn.org' --noinput
-
-# This has to be done every time you want to start a server
-#cd /docker_booktype
-source /docker_booktype/bin/activate
-source ${instancename}/booktype.env
-django-admin.py runserver 0.0.0.0:8080
+cd /${instancename}/
+/${instancename}/manage.py
+runserver 0.0.0.0:8080
